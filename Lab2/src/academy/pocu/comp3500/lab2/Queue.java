dc905f4fe33451c7c7e11b5a4a9866d76997c26c
home <autoba9687@gmail.com>
2022-01-10 16:27:05 +0900

    lab1 init

---
+++ Lab2/src/academy/pocu/comp3500/lab2/Queue.java
@@ -0,0 +1,19 @@
+package academy.pocu.comp3500.lab2;
+
+public class Queue {
+    public void enqueue(final int data) {
+
+    }
+
+    public int peek() {
+        return -1;
+    }
+
+    public int dequeue() {
+        return -1;
+    }
+
+    public int getSize() {
+        return -1;
+    }
+}
