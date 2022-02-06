fda4c5e40e86a4a1b91c7b42470c91fce3f6ac11
DESKTOP-37M41BA\johuyn <autoba9687@gmail.com>
2022-02-07 03:31:55 +0900

    implement bank get instance

--- Lab5/src/academy/pocu/comp3500/lab5/Bank.java
+++ Lab5/src/academy/pocu/comp3500/lab5/Bank.java
@@ -1,11 +1,31 @@
 package academy.pocu.comp3500.lab5;
 
 public class Bank {
-    public Bank(byte[][] pubKeys, final long[] amounts) {
+    private byte[][] mPubkeys;
+    private long[] mAmounts;
 
+    public Bank(byte[][] pubKeys, final long[] amounts) {
+        this.mPubkeys = pubKeys;
+        this.mAmounts = amounts;
     }
 
     public long getBalance(final byte[] pubKey) {
+
+        for (int i = 0; i < mPubkeys.length; i++) {
+            boolean isNoAccount = false;
+
+            for (int j = 0; j < mPubkeys[i].length; j++) {
+                if (mPubkeys[i][j] !=  pubKey[j]) {
+                    isNoAccount = true;
+                    break;
+                }
+            }
+
+            if (isNoAccount == false) {
+                return mAmounts[i];
+            }
+        }
+
         return -1;
     }
 
